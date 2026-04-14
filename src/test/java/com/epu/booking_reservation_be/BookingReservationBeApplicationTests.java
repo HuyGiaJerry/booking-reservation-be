cff89909e7d3210ac42224ee1b8b6f669b6c8109
HuyGiaJerry <157083572+HuyGiaJerry@users.noreply.github.com>
2026-04-14 10:57:06 +0700

    Khoi tao project

---
+++ src/test/java/com/epu/booking_reservation_be/BookingReservationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epu.booking_reservation_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingReservationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
